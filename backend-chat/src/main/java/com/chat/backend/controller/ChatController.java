6defc0ec638a3a431eb8744dfbb15f67f7b144a7
nicolasgilsanchezremoto <nicolasgilsanchezremoto@gmail.com>
2026-05-06 02:09:33 -0500

    Add files via upload

---
+++ backend-chat/src/main/java/com/chat/backend/controller/ChatController.java
@@ -0,0 +1,16 @@
+package com.chat.backend.controller;
+
+import com.chat.backend.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(ChatMessage chatMessage) {
+        return chatMessage;
+    }
+}
